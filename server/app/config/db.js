c36edba84724819d1ec3d3a04cf2e9458fbddd8a
Aryan10 <aryan.shivam.10@gmail.com>
2024-09-16 22:33:52 +0530

    chore: Updated .env configurations - Added .env.example files - Added default values for process.env.* - Removed excessive dotenv configurations

--- server/app/config/db.js
+++ server/app/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const options = {};
-mongoose.connect(process.env.MONGODB_URI, options);
+mongoose.connect(process.env.MONGODB_URI|| 'mongodb://localhost:27017/transformodocs', options);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
