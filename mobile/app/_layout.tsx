80620a4c4862316ce8e89fa50325c0b532a820cf
Nishant <nishant040305@gmail.com>
2024-09-16 22:47:55 +0530

    feat: Add different options in mobile application Mobile
    Convert - to covert to machine readable
    Upload - to upload machine readable
    convertImage - to convert image
    download - to download converted data

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="Index" options={{ title: "Home" }}   />
+      <Stack.Screen name="UploadMachineReadable" options={{ title: "Upload Machine Readable" }}  ></Stack.Screen>
     </Stack>
   );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
           source={require("../assets/images/convert-icon.png")} // Replace with your image path
           style={styles.buttonImage}
         />
-        <Text style={styles.buttonText}>Convert Non-Machine Readable to Readable</Text>
+        <Text style={styles.Text}>Convert</Text>
       </TouchableOpacity>
 
       {/* Button for Upload Machine Readable */}
@@ -23,7 +23,23 @@ const Index = () => {
           source={require("../assets/images/upload-icon.png")} // Replace with your image path
           style={styles.buttonImage}
         />
-        <Text style={styles.buttonText}>Upload Machine Readable</Text>
+        <Text style={styles.Text}>Upload</Text>
+      </TouchableOpacity>
+
+      {/* Button for Convert Image */}
+      <TouchableOpacity style={styles.button} onPress={() => router.push('/UploadMachineReadable')}>
+        <Image
+          source={require("../assets/images/imageConvert.png")} // Replace with your image path
+          style={styles.icon}
+        />
+        <Text style={styles.Text}>Convert Image</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => router.push('/UploadMachineReadable')}>
+        <Image
+          source={require("../assets/images/download.png")} // Replace with your image path
+          style={styles.icon}
+        />
+        <Text style={styles.Text}>Download</Text>
       </TouchableOpacity>
     </View>
   );
@@ -32,34 +48,40 @@ const Index = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
     backgroundColor: "#fff",
-  },
-  title: {
-    fontSize: 18,
-    marginBottom: 20,
-    textAlign: "center",
+    paddingTop: 100,
+
+    paddingLeft: 50,
   },
   button: {
-    flexDirection: "row",
+    flexDirection: "column",
     alignItems: "center",
-    backgroundColor: "#4CAF50",
     padding: 10,
     borderRadius: 5,
     marginBottom: 20,
-    width: 300,
+    width: 100,
+    height: 100,
     justifyContent: "center",
+    margin: 10, // Add margin to create space between buttons
   },
   buttonImage: {
-    width: 30,
-    height: 30,
-    marginRight: 10,
+    width: 50,
+    height: 50,
+    marginBottom: 10, // Adjust margin between image and text
   },
   buttonText: {
     color: "#fff",
     fontSize: 16,
   },
+  Text: {
+    fontSize: 18,
+  },
+  icon:{
+    width:70,
+    height:70,
+  }
 });
 
 export default Index;
